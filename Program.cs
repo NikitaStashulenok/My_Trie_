978becfee4e591b6fdbdf88fec5f19c2a935be3d
Nikita Stashulenok <nstashylenok@mail.ru>
2021-02-25 21:06:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace My_Trie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trie = new Trie<int>();
+            trie.Add("привет", 500);
+            trie.Add("пока", 250);
+            trie.Add("прелюдия", 50);
+            trie.Add("машина", 600);
+            trie.Add("маша", 1000);
+            trie.Add("моль", 20);
+            trie.Add("кирпич", 59);
+            trie.Add("кирка", 48);
+
+            trie.Remove("прелюдия");
+            trie.Remove("моль");
+            trie.Remove("лошадь");
+
+
+            Search(trie, "кирка");
+            Search(trie, "прелюдия");
+            Search(trie, "каша");
+            Console.ReadKey();
+        }
+
+        private static void Search(Trie<int> trie, string word)
+        {
+            if (trie.TrySearch(word + " ", out int value))
+            {
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine($"Не найдено :( -> {word}");
+            }
+        }
+    }
+}
---
+++ Trie.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My_Trie
+{
+    class Trie<T>
+    {
+        private readonly Node<T> Root;
+        public int Count { get; set; }
+
+        public Trie()
+        {
+            Root = new Node<T>('\0', default, "");
+            Count = 1;
+        }
+        
+        public void Add(string key, T data)
+        {
+            AddNode(key, data, Root);
+        }
+        private void AddNode(string key, T data, Node<T> node)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                if (!node.IsWord)
+                {
+                    node.Data = data;
+                    node.IsWord = true;
+                }
+            }
+            else
+            {
+                var symbol = key[0];
+                var subnode = node.TryFind(symbol);
+                if (subnode != null) 
+                {
+                    AddNode(key[1..], data, subnode);
+                }
+                else
+                {
+                    var newNode = new Node<T>(key[0], data, node.Prefix + key[0]);
+                    node.SubNodes.Add(key[0], newNode);
+                    AddNode(key[1..], data, newNode);
+                }
+            }
+        }
+
+        public void Remove(string key)
+        {
+            RemoveNode(key, Root);
+        }
+        private void RemoveNode(string key, Node<T> node)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                if (node.IsWord)
+                {
+                    node.IsWord = false;
+                    return;
+                }
+            }
+            else
+            {
+                var subnode = node.TryFind(key[0]);
+                if (subnode != null)
+                    RemoveNode(key[1..], subnode);
+            }
+        }
+        public bool TrySearch(string key, out T value)
+        {
+            return SearchNode(key, Root, out value);
+        }
+        private bool SearchNode(string key, Node<T> node, out T value)
+        {
+            value = default;
+            var result = false;
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                if (node.IsWord)
+                {
+                    value = node.Data;
+                    result = true;
+                }
+            }
+            else
+            {
+                var subnode = node.TryFind(key[0]);
+                if (subnode != null)
+                {
+                   result = SearchNode(key[1..], subnode, out value);
+                }
+            }
+            return result;
+        }
+    }
+}
